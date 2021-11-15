9c11f8429052965454ac66dd31d7e4520ddd1833
diogosalmeida <diogodsalmeida@gmail.com>
2021-11-14 21:57:00 -0300

    Create docker compose, create migrations and config connections with DB

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"about-me-back-end/db"
 	"about-me-back-end/routes"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+
+	db.ConnectDB()
+
 	app := fiber.New()
 
 	routes.HandlerRouterPost(app)
--- types/tags.go
+++ types/tags.go
@@ -6,9 +6,11 @@ import (
 	"gorm.io/gorm"
 )
 
+// Tags public graph issues
 type Tags struct {
 	gorm.Model
 	ID        uint `gorm:"primaryKey"`
+	PostRefer uint
 	Subject   string
 	CreatedAt time.Time
 	UpdatedAt time.Time
